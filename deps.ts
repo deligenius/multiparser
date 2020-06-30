46ee2bab7d1a870236cd8e743864b1592cfab614
gjuoun <gjuoun@gmail.com>
2020-06-30 19:21:35 -0300

    use std@0.59.0

--- deps.ts
+++ deps.ts
@@ -1,14 +1,7 @@
-// export { ServerRequest } from "https://deno.land/std@0.59.0/http/server.ts";
-export { ServerRequest } from "./std/http/server.ts";
-
-// TODO: fix "malformed MIME header line" from std/textproto/mod.ts
-// export {
-//   MultipartReader,
-//   FormFile,
-// } from "https://deno.land/std@0.59.0/mime/multipart.ts";
-
+export { ServerRequest } from "https://deno.land/std@0.59.0/http/server.ts";
 
 export {
   MultipartReader,
   FormFile,
-} from "./std/mime/multipart.ts";
+} from "https://deno.land/std@0.59.0/mime/multipart.ts";
+
